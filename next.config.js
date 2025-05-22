28d254f5db4fb7bf701c1756d4141fe480ed1a2d
Dave Mainville <dave@davemainville.com>
2025-05-21 22:19:19 -0400

    chore: configure polling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        return {
+            ...config,
+            watch: true,
+            watchOptions: {
+                aggregateTimeout: 1000,
+                poll: 1000, // Check for changes every second
+                ignored: ['node_modules', '.next']
+            }
+        }
+    },
+}
 
 module.exports = nextConfig
