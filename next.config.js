4dd671a93d41ab8d57802efa3526e146a1396e2b
Dave Mainville <dave@davemainville.com>
2025-05-29 01:18:06 -0400

    remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        return {
-            ...config,
-            watch: true,
-            watchOptions: {
-                aggregateTimeout: 1000,
-                poll: 1000, // Check for changes every second
-                ignored: ['node_modules', '.next']
-            }
-        }
-    },
-}
-
+const nextConfig = {}
 module.exports = nextConfig
